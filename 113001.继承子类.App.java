8bf012e8059881c9e5038eef795c8a00eca35c1f
ESemotionalswitch <74774285+ESemotionalswitch@users.noreply.github.com>
2020-11-30 15:39:16 +0800

    113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,15 @@
+package pac;
+
+public class APP extends R {
+	public static void main(String[] args) {
+		R r = new R();
+	    System.out.println(r.boy_filePath);
+	    System.out.println(r.girl_filePath);
+	    for (int i=0;i<r.array2d.length;i++){
+	        for (int j=0;j<r.array2d[i].length;j++){
+	            System.out.print(r.array2d[i][j]+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+	}
