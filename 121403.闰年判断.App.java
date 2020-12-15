38edac094c08c171cc3ede8fab4e4ce63ed73329
ESemotionalswitch <74774285+ESemotionalswitch@users.noreply.github.com>
2020-12-15 11:53:11 +0800

    121403.闰年判断.App.java

--- 121403.闰年判断.App.java
+++ 121403.闰年判断.App.java
@@ -1,25 +1,25 @@
-package pac;
-
+package  pac;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("请输入年份");
-        int year;//接收输入的年份
-        Scanner scanner = new Scanner(System.in);
-        year = scanner.nextInt();
-        if (year < 0) {
-            if (year%4==1&&year%100!=0||year%400==1){
-                System.out.println("公元前" + year + "是闰年");
-            }else {
-                System.out.println("公元前" + year + "不是闰年");
-            }
-        }else if (year > 3200) {
-            if(year%4==0&&year%100!=0||year%400==0){
-                System.out.println("公元" + year + "是闰年");
-            }else {
-                System.out.println("公元" + year + "不是闰年");
-            }
+        @SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+//接收用户输入
+        System.out.print("请输入一个年份：");
+        int year = input.nextInt();
+//判断是否是闰年
+        if (year > 0 && year < 3200) {
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+                System.out.println(year + "是闰年");
+            else
+                System.out.println(year + "不是闰年");
+        }
+        if (year < 0 && year> -3200) {
+            if (year % 400 == -1 || (year % 4 == -1 && year % 100 != 0))
+                System.out.println(year + "是闰年");
+            else
+                System.out.println(year + "不是闰年");
         }
     }
-}
+    }
